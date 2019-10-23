d2d237c4b8dbc672211ac6a5001b7accea8476a6
Matti Luukkainen <mluukkai@iki.fi>
2019-10-23 23:26:12 +0300

    remove comment

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -34,7 +34,6 @@ public class VarastoTest {
     public void lisaysLisaaSaldoa() {
         varasto.lisaaVarastoon(8);
 
-        // saldon pitäisi olla sama kun lisätty määrä
         assertEquals(8, varasto.getSaldo(), vertailuTarkkuus);
     }
 
