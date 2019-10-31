4e2ec0d3bf7c2742d260afcc8e63c962d34ea3fc
Matti Luukkainen <mluukkai@iki.fi>
2019-10-31 17:56:21 +0200

    koe

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -19,6 +19,8 @@ public class Main {
 
         for (int i = 0; i < 1; i++) {
             for (int j = 0; i < j; j++) {
+                System.out.println("lol");
+                System.out.println("lol");
             }
         }
     }
